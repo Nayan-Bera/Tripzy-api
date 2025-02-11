ff18fe3e385db0ee6acd40ebf48c43e63cbcd398
Nayan Bera <nayanbera9@gmail.com>
2025-02-12 00:06:57 +0530

    first commit

---
+++ src/drizzle.config.ts
@@ -0,0 +1,34 @@
+// import { defineConfig } from 'drizzle-kit';
+// export default defineConfig({
+//     dialect: 'postgresql',
+//     schema: './src/db/schema/index.ts',
+//     dbCredentials: {
+//         // url: process.env.PG_DB_URL || '',
+//         // ssl: "require"
+//         host: "db-postgresql-syd-do-user-18866531-0.j.db.ondigitalocean.com",
+//         port: 25060,
+//         user: "doadmin",
+//         password: "AVNS_M7TSboxFxmqJiM-DkmB",
+//         database: "defaultdb",
+//         ssl: "require",
+//     },
+//     // migrations: {
+//     //     prefix: 'supabase',
+//     // },
+//     out: './src/db/migrations',
+// });
+
+
+// import { defineConfig } from 'drizzle-kit';
+// export default defineConfig({
+//     dialect: 'postgresql',
+//     schema: './src/db/schema/index.ts',
+//     dbCredentials: {
+//         url: process.env.PG_DB_URL || '',
+//     },
+
+//     // migrations: {
+//     //     prefix: 'supabase',
+//     // },
+//     out: './src/db/migrations',
+// });
