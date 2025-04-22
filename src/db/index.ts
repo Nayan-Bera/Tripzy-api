3a24e52a86ad77c76e3454c3b626676d61a44a96
Nayan Bera <nayanbera9@gmail.com>
2025-04-23 00:20:13 +0530

    feat:change user

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,14 +1,9 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import {
-
-    user,
-} from './schema';
-
+import { user } from './schema';
 
 const schema = {
     user,
-    
 } as const;
 
 export const sql = postgres(process.env.PG_DB_URL || '', { max: 1 });
