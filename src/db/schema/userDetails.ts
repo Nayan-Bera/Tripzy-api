60da58bc4401e5585a7340994e83725015c4450d
Nayan Bera <nayanbera9@gmail.com>
2025-04-25 23:51:45 +0530

    user details

--- src/db/schema/userDetails.ts
+++ src/db/schema/userDetails.ts
@@ -15,6 +15,13 @@ const userDetails = pgTable('user-details', {
         onUpdate: 'no action',
     }),
     pan_number:varchar('pan_number').notNull(),
+    aadhar_number:varchar('aadhar_number').notNull(),
+    address:varchar('address').notNull(),
+    city:varchar('city').notNull(),
+    state:varchar('state').notNull(),
+    country:varchar('country').notNull(),
+    pincode:varchar('pincode').notNull(),
+    
    
     createdAt: timestamp('created_at', { mode: 'string' })
         .notNull()
