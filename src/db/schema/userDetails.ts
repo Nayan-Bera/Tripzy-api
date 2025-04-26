17a8b8a2a23b899b9de7b4c6d3f544ed8dbda82a
Nayan Bera <nayanbera9@gmail.com>
2025-04-26 23:34:37 +0530

    feat user details

--- src/db/schema/userDetails.ts
+++ src/db/schema/userDetails.ts
@@ -1,4 +1,4 @@
-import { relations } from 'drizzle-orm';
+import { is, relations } from 'drizzle-orm';
 import {
     boolean,
     pgTable,
@@ -14,15 +14,20 @@ const userDetails = pgTable('user-details', {
         onDelete: 'cascade',
         onUpdate: 'no action',
     }),
-    pan_number:varchar('pan_number').notNull(),
-    aadhar_number:varchar('aadhar_number').notNull(),
-    address:varchar('address').notNull(),
-    city:varchar('city').notNull(),
-    state:varchar('state').notNull(),
-    country:varchar('country').notNull(),
-    pincode:varchar('pincode').notNull(),
-    
-   
+    pan_number:varchar('pan_number'),
+    gst_number:varchar('gst_number'),
+    passport_number:varchar('passport_number'),
+    aadhar_number:varchar('aadhar_number'),
+    address:varchar('address'),
+    city:varchar('city'),
+    state:varchar('state'),
+    country:varchar('country'),
+    pincode:varchar('pincode'),
+    is_verified: boolean('is_verified').default(false),
+    ducuments:varchar('ducuments'),
+    documents_type:varchar('documents_type'{
+        enum: ['pan', 'aadhar', 'passport'],
+    }),
     createdAt: timestamp('created_at', { mode: 'string' })
         .notNull()
         .defaultNow(),
