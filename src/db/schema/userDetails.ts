657a7825f34c50b83ccd81e8753b91e3e4fd5337
Nayan Bera <nayanbera9@gmail.com>
2025-04-26 23:35:26 +0530

    feat user details

--- src/db/schema/userDetails.ts
+++ src/db/schema/userDetails.ts
@@ -10,22 +10,24 @@ import user from './user';
 
 const userDetails = pgTable('user-details', {
     id: uuid('id').defaultRandom().primaryKey(),
-    user_id:uuid('user_id').notNull().references(() => user.id, {
-        onDelete: 'cascade',
-        onUpdate: 'no action',
-    }),
-    pan_number:varchar('pan_number'),
-    gst_number:varchar('gst_number'),
-    passport_number:varchar('passport_number'),
-    aadhar_number:varchar('aadhar_number'),
-    address:varchar('address'),
-    city:varchar('city'),
-    state:varchar('state'),
-    country:varchar('country'),
-    pincode:varchar('pincode'),
+    user_id: uuid('user_id')
+        .notNull()
+        .references(() => user.id, {
+            onDelete: 'cascade',
+            onUpdate: 'no action',
+        }),
+    pan_number: varchar('pan_number'),
+    gst_number: varchar('gst_number'),
+    passport_number: varchar('passport_number'),
+    aadhar_number: varchar('aadhar_number'),
+    address: varchar('address'),
+    city: varchar('city'),
+    state: varchar('state'),
+    country: varchar('country'),
+    pincode: varchar('pincode'),
     is_verified: boolean('is_verified').default(false),
-    ducuments:varchar('ducuments'),
-    documents_type:varchar('documents_type'{
+    ducuments: varchar('ducuments'),
+    documents_type: varchar('documents_type', {
         enum: ['pan', 'aadhar', 'passport'],
     }),
     createdAt: timestamp('created_at', { mode: 'string' })
@@ -37,10 +39,10 @@ const userDetails = pgTable('user-details', {
 });
 
 export const userDetailsRelations = relations(userDetails, ({ one, many }) => ({
-   user:one(user, {
-       fields: [userDetails.user_id],
-       references: [user.id],
-   })
+    user: one(user, {
+        fields: [userDetails.user_id],
+        references: [user.id],
+    }),
 }));
 
 export default user;
