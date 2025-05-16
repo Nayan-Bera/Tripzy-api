a51204d86110e10c0f4c952a4670d7a3891c3a21
Nayan Bera <nayanbera9@gmail.com>
2025-05-16 22:45:29 +0530

    feat:schemas

--- src/db/index.ts
+++ src/db/index.ts
@@ -17,6 +17,20 @@ import {
     userCoupons,
     userdetails,
 } from './schema';
+import { userRelations } from './schema/user';
+import { propertiesRelations } from './schema/properties';
+import { roomsRelations } from './schema/rooms';
+import { roomImagesRelations } from './schema/roomsImages';
+import { roomAvailabilityRelations } from './schema/roomavailable';
+import { couponsRelations } from './schema/coupons';
+import { favoritesRelations } from './schema/favourites';
+import { notificationsRelations } from './schema/notifications';
+import { userDetailsRelations } from './schema/userDetails';
+import { propertyImagesRelations } from './schema/PropertiesImages';
+import { paymentsRelations } from './schema/payments';
+import { reviewsRelations } from './schema/review';
+import { userCouponsRelations } from './schema/userCoupons';
+import { digitalVerificationRelations } from './schema/digitelverification';
 
 const schema = {
     user,
@@ -34,7 +48,20 @@ const schema = {
     roomImages,
     roomAvailability,
     digitalVerification,
-    
+    userRelations,
+    propertiesRelations,
+    roomsRelations,
+    roomImagesRelations,
+    roomAvailabilityRelations,
+    couponsRelations,
+    favoritesRelations,
+    notificationsRelations,
+    propertyImagesRelations,
+    paymentsRelations,
+    reviewsRelations,
+    userCouponsRelations,
+    userDetailsRelations,
+    digitalVerificationRelations,
 } as const;
 
 export const sql = postgres(process.env.PG_DB_URL || '', { max: 1 });
