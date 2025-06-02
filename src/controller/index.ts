2af686be5b49ba2d5bc603d0a90b7c051ce483f4
Nayan Bera <nayanbera9@gmail.com>
2025-06-02 22:21:37 +0530

    .

---
+++ src/controller/index.ts
@@ -0,0 +1,4 @@
+export {default as registerController} from './v1/auth/register.controller';
+export {default as loginController} from './v1/auth/login.controller';
+export {default as refreshTokenController} from './v1/auth/refreshToken.controller';
+export {default as logoutController} from './v1/auth/logout.controller';
