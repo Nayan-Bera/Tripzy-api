da29efc4fbefd03879339463564203b194742210
Nayan Bera <nayanbera9@gmail.com>
2025-12-11 21:33:50 +0530

    fixed the backend

--- dist/utils/generaterandomDigit.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const generateRandomDigit = (startWith, digit) => {
-    return Math.floor(startWith + Math.random() * digit);
-};
-exports.default = generateRandomDigit;
