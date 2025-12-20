f6bf36688b2b1a5cde647ca3ce57af2fd926876c
Nayan Bera <nayanbera9@gmail.com>
2025-12-20 23:55:24 +0530

    role controllers

--- src/routes/role/role.route.ts
+++ src/routes/role/role.route.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { roleController } from '../../controller';
 const router = Router();
-router.get('/role', roleController.getRole);
-router.post('/role', roleController.addrole);
+router.get('/get-role', roleController.getRole);
+router.post('/add-role', roleController.addrole);
 router.put('/role/:id', roleController.updateRole);
 export default router;
