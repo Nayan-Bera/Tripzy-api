714d20d5ea9823591a992210f9ceb78f4e9c8ef0
Nayan Bera <nayanbera9@gmail.com>
2025-12-31 01:01:18 +0530

    add the seed

--- src/db/seed/index.ts
+++ src/db/seed/index.ts
@@ -1,14 +1,26 @@
-import { seedPermissions } from "./permissions.seed";
-import { seedRoles } from "./roles.seed";
-import { seedRolePermissions } from "./rolePermissions.seed";
-import usersHotelsSeeder  from "./users.seed";
+import { seedPermissions } from './permissions.seed';
+import { seedRoles } from './roles.seed';
+import { seedRolePermissions } from './rolePermissions.seed';
+import usersHotelsSeeder from './users.seed';
 
 async function runSeeds() {
-  await seedPermissions();
-  await seedRoles();
-  await seedRolePermissions();
-  await usersHotelsSeeder.run();
-  process.exit(0);
+    await seedPermissions();
+    await seedRoles();
+    await seedRolePermissions();
+    await usersHotelsSeeder.run();
+    process.exit(0);
 }
 
 runSeeds();
+
+// Email	Password
+// super@demo.com
+// 	Abc@123
+// admin@demo.com
+// 	Abc@123
+// user@demo.com
+// 	Abc@123
+// owner@demo.com
+// 	Abc@123
+// staff@demo.com
+// 	Abc@123
